c2c471fee47bd3c25ef9d473a5032140fae02e34
Natan <nfigueredobalbo@gmail.com>
2024-04-17 15:37:54 -0300

    atualização

--- src/controllers/categoriaController.ts
+++ src/controllers/categoriaController.ts
@@ -3,29 +3,49 @@ import categoriaService from '../service/categoriaService'
 
 class categoriaController {
     async create(req: Request, res: Response) {
+       try{
         const createdCategoria = await categoriaService.create(req.body)
         res.status(201)
         return res.json(createdCategoria)
+       }catch(error){
+        console.error(error);
+       }
     }   
 
     async findAll(req: Request, res: Response) {
-        const findedCategorias= await categoriaService.findAll()
-        return res.json(findedCategorias)
+        try{
+            const findedCategorias= await categoriaService.findAll()
+            return res.json(findedCategorias)
+        }catch(error){
+            console.error(error);
+       }
     }
 
     async findById(req: Request, res: Response) {
-        const findedCategoria = await categoriaService.findById(req.params.id)
-        return res.json(findedCategoria)
+        try{
+            const findedCategoria = await categoriaService.findById(req.params.id)
+            return res.json(findedCategoria)
+        }catch(error){
+            console.error(error);
+       }
     }
 
     async update(req: Request, res: Response) {
-        const updatedCategoria= await categoriaService.update(req.params.id, req.body)
-        return res.json(updatedCategoria)
+        try{
+            const updatedCategoria= await categoriaService.update(req.params.id, req.body)
+            return res.json(updatedCategoria)
+        }catch(error){
+            console.error(error);
+        }     
     }
 
     async delete(req: Request, res: Response) {
-        const deleteMessage = await categoriaService.delete(req.params.id)
-        return res.json(deleteMessage)
+        try{
+            const deleteMessage = await categoriaService.delete(req.params.id)
+            return res.json(deleteMessage)
+        }catch(error){
+            console.error(error);
+        }
     }
 }
 
